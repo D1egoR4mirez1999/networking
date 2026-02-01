1d12728125464101d1a695793b7fce1f92bd252f
Diego Ramirez <dialrasa2@gmail.com>
2026-02-01 17:31:26 -0500

    improving the ui of the chat app

--- chat-app/client.js
+++ chat-app/client.js
@@ -6,11 +6,35 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const clearLine = (dir) => {
+  return new Promise((resolve) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
+
+const moveCursor = (left, top) => {
+  return new Promise((resolve) => {
+    process.stdout.moveCursor(left, top, () => {
+      resolve();
+    });
+  });
+};
+
+const ask = async () => {
+  const message = await rl.question("Enter a message> ");
+
+  await moveCursor(0, -1);
+  await clearLine(0);
+  
+  client.write(message);
+};
+
 const client = net.createConnection(
   { host: "127.0.0.1", port: 3000 },
   async () => {
-    const message = await rl.question("Enter a message:");
-    client.write(message);
+    ask();
   }
 );
 
@@ -18,6 +42,12 @@ client.on("end", () => {
   console.log("Connection closed");
 });
 
-client.on("data", (data) => {
-  console.log("Message: ", data.toString());
+client.on("data", async (data) => {
+  console.log();
+  await moveCursor(0, -1);
+  await clearLine(0);
+
+  console.log(data.toString());
+
+  ask();
 });
