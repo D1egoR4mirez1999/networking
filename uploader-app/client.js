8f5290bcb1e72ef0bed0824127c4a2c26908d40e
Diego Ramirez <dialrasa2@gmail.com>
2026-02-11 18:12:17 -0500

    adding logic to handle file upload

--- uploader-app/client.js
+++ uploader-app/client.js
@@ -1,18 +1,32 @@
 const net = require("node:net");
 const fs = require("node:fs/promises");
+const path = require("node:path");
 
 const PORT = "4020";
 const HOST = "::1";
 
 const client = net.createConnection({ host: HOST, port: PORT }, async () => {
-  const sourceFileHandle = await fs.open("source.txt", "r");
-  const sourceFile = sourceFileHandle.createReadStream();
-
-  sourceFile.on("data", (data) => {
-    client.write(data);
+  const filePath = process.argv[2];
+  const fileName = path.basename(filePath);
+  
+  client.write(`filaName: ${fileName}-------`);
+  
+  const sourceFileHandler = await fs.open(filePath, "r");
+  const sourceFileStream = sourceFileHandler.createReadStream();
+  
+  sourceFileStream.on("data", (data) => {
+    const canContinue = client.write(data);
+    
+    if (!canContinue) {
+      sourceFileStream.pause();
+    }
   });
 
-  sourceFile.on("end", () => {
+  client.on("drain", () => {
+    sourceFileStream.resume();
+  });
+
+  sourceFileStream.on("end", () => {
     client.end();
   });
 });
--- uploader-app/server.js
+++ uploader-app/server.js
@@ -2,22 +2,57 @@ const net = require("node:net");
 const fs = require("node:fs/promises");
 
 const PORT = "4020";
+
+let socketInstance = null;
+let destinationFileHandler = null;
+let destinationFileStream = null;
+
+const handleFileUpload = async (data) => {
+  if (!destinationFileHandler) {
+    const indexOfSeparator = data.indexOf("-------");
+    const fileName = data.subarray(10, indexOfSeparator).toString();
+    const dataToWrite = data.subarray(indexOfSeparator + 7);
+    
+    socketInstance.pause();
+    destinationFileHandler = await fs.open(`./storage/${fileName}`, "w");
+    destinationFileStream = destinationFileHandler.createWriteStream();
+    destinationFileStream.write(dataToWrite);
+    socketInstance.resume();
+
+    destinationFileStream.on("drain", () => {
+      socketInstance.resume();
+    });
+
+    return;
+  }
+
+  const canContinue = destinationFileStream.write(data);
+
+  if (!canContinue) {
+    socketInstance.pause();
+  }
+};
+
 const server = net.createServer();
-
-server.on("connection", async (socket) => {
-  const destinationFileHandle = await fs.open("./storage/destination.txt", "w");
-  const destinationFile = destinationFileHandle.createWriteStream();
-
-  socket.on("data", (data) => {
-    destinationFile.write(data);
-  });
-
-  socket.on("end", () => {
-    destinationFileHandle.close();
-    console.log("File uploaded successfully");
-  });
-});
 
 server.listen(PORT, () => {
   console.log("Server is running on ", server.address());
+});
+
+server.on("connection", async (socket) => {
+  console.log("New connection established");
+  
+  socketInstance = socket;
+  socketInstance.on("data", (data) => {
+    handleFileUpload(data);
+  });
+  
+  socketInstance.on("end", () => {
+    console.log("File uploaded successfully");
+  
+    destinationFileHandler.close();
+    socketInstance = null;
+    destinationFileHandler = null;
+    destinationFileStream = null;
+  });
 });
