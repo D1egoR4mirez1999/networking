0eaefb7ab0848f5ca7481fd799883b22aaf392c7
Diego Ramirez <dialrasa2@gmail.com>
2026-02-12 07:11:55 -0500

    improve ui showing the file upload percentage

--- uploader-app/client.js
+++ uploader-app/client.js
@@ -5,6 +5,22 @@ const path = require("node:path");
 const PORT = "4020";
 const HOST = "::1";
 
+const clearLine = (dir) => {
+  return new Promise((resolve) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
+
+const moveCursor = (left, top) => {
+  return new Promise((resolve) => {
+    process.stdout.moveCursor(left, top, () => {
+      resolve();
+    });
+  });
+};
+
 const client = net.createConnection({ host: HOST, port: PORT }, async () => {
   const filePath = process.argv[2];
   const fileName = path.basename(filePath);
@@ -13,9 +29,21 @@ const client = net.createConnection({ host: HOST, port: PORT }, async () => {
   
   const sourceFileHandler = await fs.open(filePath, "r");
   const sourceFileStream = sourceFileHandler.createReadStream();
+  const sourceFileSize = (await sourceFileHandler.stat()).size;
   
-  sourceFileStream.on("data", (data) => {
+  let bytesWritten = 0;
+  let percentage = 0;
+  console.log();
+  
+  sourceFileStream.on("data", async (data) => {
     const canContinue = client.write(data);
+
+    bytesWritten += data.length;
+    percentage = Math.round((bytesWritten / sourceFileSize) * 100);
+    
+    await moveCursor(0, -1);
+    await clearLine(0);
+    console.log(`Uploading... ${percentage}%`);
     
     if (!canContinue) {
       sourceFileStream.pause();
--- uploader-app/server.js
+++ uploader-app/server.js
@@ -2,37 +2,6 @@ const net = require("node:net");
 const fs = require("node:fs/promises");
 
 const PORT = "4020";
-
-let socketInstance = null;
-let destinationFileHandler = null;
-let destinationFileStream = null;
-
-const handleFileUpload = async (data) => {
-  if (!destinationFileHandler) {
-    const indexOfSeparator = data.indexOf("-------");
-    const fileName = data.subarray(10, indexOfSeparator).toString();
-    const dataToWrite = data.subarray(indexOfSeparator + 7);
-    
-    socketInstance.pause();
-    destinationFileHandler = await fs.open(`./storage/${fileName}`, "w");
-    destinationFileStream = destinationFileHandler.createWriteStream();
-    destinationFileStream.write(dataToWrite);
-    socketInstance.resume();
-
-    destinationFileStream.on("drain", () => {
-      socketInstance.resume();
-    });
-
-    return;
-  }
-
-  const canContinue = destinationFileStream.write(data);
-
-  if (!canContinue) {
-    socketInstance.pause();
-  }
-};
-
 const server = net.createServer();
 
 server.listen(PORT, () => {
@@ -41,17 +10,44 @@ server.listen(PORT, () => {
 
 server.on("connection", async (socket) => {
   console.log("New connection established");
-  
-  socketInstance = socket;
-  socketInstance.on("data", (data) => {
+
+  let destinationFileHandler = null;
+  let destinationFileStream = null;
+
+  const handleFileUpload = async (data) => {
+    if (!destinationFileHandler) {
+      const indexOfSeparator = data.indexOf("-------");
+      const fileName = data.subarray(10, indexOfSeparator).toString();
+      const dataToWrite = data.subarray(indexOfSeparator + 7);
+
+      socket.pause();
+      destinationFileHandler = await fs.open(`./storage/${fileName}`, "w");
+      destinationFileStream = destinationFileHandler.createWriteStream();
+      destinationFileStream.write(dataToWrite);
+      socket.resume();
+
+      destinationFileStream.on("drain", () => {
+        socket.resume();
+      });
+
+      return;
+    }
+
+    const canContinue = destinationFileStream.write(data);
+
+    if (!canContinue) {
+      socket.pause();
+    }
+  };
+
+  socket.on("data", (data) => {
     handleFileUpload(data);
   });
-  
-  socketInstance.on("end", () => {
+
+  socket.on("end", () => {
     console.log("File uploaded successfully");
-  
+
     destinationFileHandler.close();
-    socketInstance = null;
     destinationFileHandler = null;
     destinationFileStream = null;
   });
